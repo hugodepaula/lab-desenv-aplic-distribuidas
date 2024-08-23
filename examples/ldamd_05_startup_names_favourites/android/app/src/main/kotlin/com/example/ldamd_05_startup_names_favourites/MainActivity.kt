63b517daa94bb52241173f041874f3091b031049
Hugo de Paula <hugo@pucminas.br>
2024-08-22 21:20:07 -0300

    Deleted old flutter examples

--- examples/ldamd_05_startup_names_favourites/android/app/src/main/kotlin/com/example/ldamd_05_startup_names_favourites/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ldamd_05_startup_names_favourites
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/ldamd_11_flutter_geolocation/android/app/src/main/kotlin/com/example/ldamd_11_flutter_geolocation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ldamd_11_flutter_geolocation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
