238aa5f77b5c9b8d9a200cc74ae123b7ec43026e
Hugo de Paula <hugo@pucminas.br>
2024-08-29 08:46:51 -0300

    Updated Registry port in rmi-compute

--- src/rmi/rmi-compute/src/main/java/br/ldamd/client/ComputePi.java
+++ src/rmi/rmi-compute/src/main/java/br/ldamd/client/ComputePi.java
@@ -9,14 +9,11 @@ import java.math.BigDecimal;
 
 public class ComputePi {
     public static void main(String args[]) {
-//        if (System.getSecurityManager() == null) {
-//            System.setSecurityManager(new SecurityManager());
-//        }
         try {
             String name = "Compute";
-            Registry registry = LocateRegistry.getRegistry(args[0]);
+            Registry registry = LocateRegistry.getRegistry("localhost",10099);
             Compute comp = (Compute) registry.lookup(name);
-            Pi task = new Pi(Integer.parseInt(args[1]));
+            Pi task = new Pi(100000);
             BigDecimal pi = comp.executeTask(task);
             System.out.println(pi);
         } catch (Exception e) {
--- src/rmi/rmi-compute/src/main/java/br/ldamd/engine/ComputeEngine.java
+++ src/rmi/rmi-compute/src/main/java/br/ldamd/engine/ComputeEngine.java
@@ -15,15 +15,13 @@ public class ComputeEngine implements Compute {
     }
 
     public static void main(String[] args) {
-        //if (System.getSecurityManager() == null) {
-        //    System.setSecurityManager(new SecurityManager());
-        //}
+
         try {
             String name = "Compute";
             Compute engine = new ComputeEngine();
             Compute stub =
                 (Compute) UnicastRemoteObject.exportObject(engine, 0);
-            Registry registry = LocateRegistry.getRegistry();
+            Registry registry = LocateRegistry.createRegistry(10099);
             registry.rebind(name, stub);
             System.out.println("ComputeEngine bound");
         } catch (Exception e) {
