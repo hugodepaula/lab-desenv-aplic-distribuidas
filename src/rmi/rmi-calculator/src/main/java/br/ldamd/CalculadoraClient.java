c4c8e0234f36918da4b47198e34ed17c7dedd5e9
José Victor <jvictormmendesd@gmail.com>
2024-09-04 20:01:11 -0300

    fix: rmi-calculator

--- src/rmi/rmi-calculator/src/main/java/br/ldamd/CalculadoraClient.java
+++ src/rmi/rmi-calculator/src/main/java/br/ldamd/CalculadoraClient.java
@@ -1,18 +1,20 @@
 package br.ldamd;
 
 
-import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.net.MalformedURLException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.NotBoundException;
 
 public class CalculadoraClient {
 
 	public static void main(String[] args) {
-		String servidor = "rmi://localhost/";
+		String servidor = "localhost";
+		int port = 10098;
 		String nome = "CalculadoraService";
 		try {
-			Calculadora c = (Calculadora) Naming.lookup(servidor + nome);
+			Registry registry = LocateRegistry.getRegistry(servidor, port);
+			Calculadora c = (Calculadora) registry.lookup(nome);
 			System.out.println("Objeto remoto \'"+ nome + "\' encontrado no servidor.");
 
 			int x = 10, y = 5;
@@ -21,8 +23,6 @@ public class CalculadoraClient {
 			System.out.println(x + " * " + y + " = " + c.multiplicar(x, y));
 			System.out.println(x + " / " + y + " = " + c.dividir(x, y));
 
-		} catch (MalformedURLException e) {
-			System.out.println("URL \'" + servidor + nome + "\' mal formatada.");
 		} catch (RemoteException e) {
 			System.out.println("Erro na invoca��o remota.");
 			e.printStackTrace();
