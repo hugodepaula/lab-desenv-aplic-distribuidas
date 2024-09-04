b6745a1132b0144f5a18048aa73aabe4f0f86fa6
José Dias <jvictormmendesd@gmail.com>
2024-09-04 20:24:07 -0300

    fix: rmi-calculator (#1)

--- src/rmi/rmi-calculator/src/main/java/br/ldamd/CalculadoraServer.java
+++ src/rmi/rmi-calculator/src/main/java/br/ldamd/CalculadoraServer.java
@@ -1,13 +1,20 @@
 package br.ldamd;
 
-import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class CalculadoraServer {
+	
 
 	public CalculadoraServer() {
+		int port = 10098;
+		String nome = "CalculadoraService";
+
 		try {
-			Calculadora c = new CalculadoraServant();
-			Naming.rebind("rmi://localhost/CalculadoraService", c);
+			Calculadora calculadora = new CalculadoraServant();
+			Registry registry = LocateRegistry.createRegistry(port);
+			registry.rebind(nome, calculadora);
+			System.out.println("Servidor Calculadora em execu��o.");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -15,6 +22,5 @@ public class CalculadoraServer {
 
 	public static void main(String args[]) {
 		new CalculadoraServer();
-		System.out.println("Servidor Calculadora em execu��o.");
 	}
 }
