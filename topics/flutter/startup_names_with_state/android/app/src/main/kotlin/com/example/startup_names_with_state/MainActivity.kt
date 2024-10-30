ca11f4566421f6219b7432aabb2ceb98adc4627f
Hugo de Paula <hugo@pucminas.br>
2024-10-30 19:38:12 -0300

    Added Flutter Startup names with state example

---
+++ topics/flutter/startup_names_with_state/android/app/src/main/kotlin/com/example/startup_names_with_state/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.startup_names_with_state
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ topics/flutter/startup_names_with_state/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Names',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Names'),
+        ),
+        body: Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override                                  
+  Widget build(BuildContext context) {
+    final WordPair wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
