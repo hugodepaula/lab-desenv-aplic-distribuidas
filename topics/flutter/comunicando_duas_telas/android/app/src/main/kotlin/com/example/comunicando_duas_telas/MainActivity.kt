296b7f26186ed2f565427d02104d6884d40281b6
Hugo de Paula <hugo@pucminas.br>
2024-11-06 21:36:40 -0300

    Added flutter example comunicando_duas_telas

---
+++ topics/flutter/comunicando_duas_telas/android/app/src/main/kotlin/com/example/comunicando_duas_telas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.comunicando_duas_telas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ topics/flutter/comunicando_duas_telas/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App de Navegação',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tela Inicial'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondScreen(data: 'Olá da Tela Inicial'),
+              ),
+            );
+          },
+          child: Text('Ir para a Segunda Tela'),
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  final String data;
+
+  SecondScreen({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Segunda Tela'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              data,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Voltar para a Tela Inicial'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
