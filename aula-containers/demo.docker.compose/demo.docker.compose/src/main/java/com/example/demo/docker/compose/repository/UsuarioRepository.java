cadd85d76d38d6e71db0d84cd80d622a415f26dc
Hugo de Paula <hugo@pucminas.br>
2025-05-27 22:46:55 -0300

    feat: :sparkles: Add aula containers

--- aula-containers/demo.docker.compose/demo.docker.compose/src/main/java/com/example/demo/docker/compose/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.docker.compose.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.docker.compose.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
